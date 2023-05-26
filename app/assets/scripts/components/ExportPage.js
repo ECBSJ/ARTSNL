d74c930c19f5fd97c6fa57dbeb947783301736e4
ECBSJ <crypto67sauce@gmail.com>
2023-05-26 22:49:23 +0800

    export page enhancement

--- app/assets/scripts/components/ExportPage.js
+++ app/assets/scripts/components/ExportPage.js
@@ -1,18 +1,48 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useContext } from "react"
 import { MdArrowBack } from "react-icons/md"
+import StateContext from "../StateContext"
+import DispatchContext from "../DispatchContext"
+import * as uint8arraytools from "uint8array-tools"
+import { CopyToClipboard } from "react-copy-to-clipboard"
 
 function ExportPage({ isExportOpen, setIsExportOpen }) {
+  const appState = useContext(StateContext)
+  const appDispatch = useContext(DispatchContext)
+
+  let keyPairObject = {
+    priv: "5e6cca2c25d67950acee3324d41ebef7d886b6762eaadb92210a3604a6188110",
+    pub: "0465034a033e228fc298f4be365cdc4555b9ef4a7a53ae9f72a88383a4712095c2cad1d12366842198e6fd4a884bd5f899c3c41f0c105aed2e470b7d0993aa2a27"
+  }
+
+  function handleCopyEvent() {
+    document.getElementById("copy-element").innerText = "COPIED!"
+
+    setTimeout(() => {
+      document.getElementById("copy-element").innerText = "COPY"
+    }, 1500)
+  }
+
   return (
     <>
-      <div className="menu__cover">
+      <div className="menu__cover menu__cover--no-opacity">
         <div className="menu__label">
           <MdArrowBack onClick={() => setIsExportOpen(!isExportOpen)} className="icon" />
-          EXPORT
+          <div>EXPORT</div>
+          <div className="menu__label__sub-label">Your private & public key pair are displayed below. Back them up in a secure location or import them into another wallet.</div>
         </div>
         <div className="menu__dashboard">
-          <div className="menu__dashboard-row--thick"></div>
+          <div className="menu__dashboard-row--thick">
+            <div>Private Key: "5e6cca2c25d67950acee3324d41ebef7d886b6762eaadb92210a3604a6188110"</div>
+            <br />
+            <div>Public Key: "0465034a033e228fc298f4be365cdc4555b9ef4a7a53ae9f72a88383a4712095c2cad1d12366842198e6fd4a884bd5f899c3c41f0c105aed2e470b7d0993aa2a27"</div>
+            <br />
+          </div>
           <div className="menu__dashboard-row">
-            <div className="menu__dashboard-row-box">COPY</div>
+            <CopyToClipboard text={JSON.stringify(keyPairObject)} onCopy={handleCopyEvent}>
+              <div id="copy-element" className="menu__dashboard-row-box">
+                COPY
+              </div>
+            </CopyToClipboard>
             <div className="menu__dashboard-row-box">RESET</div>
           </div>
         </div>
--- app/assets/scripts/components/ImportPage.js
+++ app/assets/scripts/components/ImportPage.js
@@ -4,7 +4,7 @@ import { MdArrowBack } from "react-icons/md"
 function ImportPage({ isImportOpen, setIsImportOpen }) {
   return (
     <>
-      <div className="menu__cover">
+      <div className="menu__cover menu__cover--no-opacity">
         <div className="menu__label">
           <MdArrowBack onClick={() => setIsImportOpen(!isImportOpen)} className="icon" />
           IMPORT
@@ -14,7 +14,9 @@ function ImportPage({ isImportOpen, setIsImportOpen }) {
             <div className="menu__dashboard-row-box">FORMAT</div>
             <div className="menu__dashboard-row-box">INFO</div>
           </div>
-          <div className="menu__dashboard-row"></div>
+          <div className="menu__dashboard-row">
+            <input className="input--position-off" type="text" />
+          </div>
           <div className="menu__dashboard-row">
             <div className="menu__dashboard-row-box">IMPORT</div>
           </div>
