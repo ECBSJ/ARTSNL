d1f142ed5940d851c25efcc461f29033f3ce2b92
ECBSJ <crypto67sauce@gmail.com>
2023-05-30 22:47:30 +0800

    wallet main testnet switch

--- app/assets/scripts/components/Main.js
+++ app/assets/scripts/components/Main.js
@@ -1,4 +1,6 @@
-import React, { useState, useTransition, useEffect, useRef } from "react"
+import React, { useState, useTransition, useEffect, useRef, useContext } from "react"
+import StateContext from "../StateContext"
+import DispatchContext from "../DispatchContext"
 import * as bitcoin from "../../../../bitcoinjs-lib"
 import * as crypto from "crypto"
 import { Link } from "react-router-dom"
@@ -18,11 +20,16 @@ import { Tooltip } from "react-tooltip"
 import ModalDropDown from "./ModalDropDown"
 import { CSSTransition } from "react-transition-group"
 import * as bip38 from "bip38"
+import { FaBitcoin, FaEthereum } from "react-icons/fa"
+import { BsHddNetworkFill, BsHddNetwork, BsReception1, BsReception4 } from "react-icons/bs"
 
 // import * as dotenv from "dotenv"
 // dotenv.config()
 
 function Main() {
+  const appState = useContext(StateContext)
+  const appDispatch = useContext(DispatchContext)
+
   const [page, setPage] = useState(1)
 
   const ECPair = ECPairFactory(ecc)
@@ -433,19 +440,13 @@ function Main() {
   //   }
   // })
 
-  const [randomBytes, setRandomBytes] = useState()
-  const [accepted, setAccepted] = useState(false)
+  const [isTestnet, setIsTestnet] = useState(false)
 
-  function handleRandomBytes() {
-    let result = crypto.randomBytes(32)
-    setRandomBytes(uint8arraytools.toHex(result))
-  }
-
-  function handleAccept() {
-    document.getElementById("accept-button").classList.toggle("button-purple")
-    document.getElementById("accept-button").disabled = true
-    setAccepted(!accepted)
-  }
+  const static_privKey = "3fdde77e8b442bc89dc890adf8fd72b4314e99ea7a205b9dd302114c9aefc493"
+  const static_publicKey = "0488a0dfca9af0d817962b25d1aa92d64e1645c94d452f6e75f61adc3f78d61b623637901afdf2efcb0bbf5badd82c2e559f22fe2f824438515614137443cb62ea"
+  const static_btc_address = "19G4UV3YDkTYj4G3XSYeUkzp4Ew6voQFiR"
+  const static_btc_testnet_address = "mon1mY8X2mtoWAjfF1X2JgD8vEXotDVsiY"
+  const static_eth_address = "0x9189561aed3229361a1aca088323a3ab0750c5d6"
 
   return (
     <>
@@ -466,18 +467,46 @@ function Main() {
           </div> */}
 
           <div className="wallet-main__overlay">
-            <div className="wallet-main__asset-display"></div>
-            <div className="wallet-main__asset-display"></div>
+            <IconContext.Provider value={{ size: "300px" }}>
+              <div className="wallet-main__asset-display wallet-main__asset-display--bitcoin">
+                {static_btc_address ? (
+                  <div className="wallet-main__asset-display--label">
+                    <div style={{ fontSize: ".4em" }}>{isTestnet ? static_btc_testnet_address : static_btc_address}</div>
+                    <div>{isTestnet ? "tBTC" : "BTC"}</div>
+                    <div style={{ fontSize: ".6em" }}>{isTestnet ? "testnet" : "mainnet"}</div>
+                  </div>
+                ) : (
+                  <div className="wallet-main__asset-display--label">
+                    <div>Create a BTC wallet.</div>
+                  </div>
+                )}
+                <FaBitcoin className="wallet-main__asset-display--bitcoin-logo" />
+              </div>
+              <div className="wallet-main__asset-display wallet-main__asset-display--ethereum">
+                {static_eth_address ? (
+                  <div className="wallet-main__asset-display--label">
+                    <div style={{ fontSize: ".4em" }}>{static_eth_address}</div>
+                    <div>{isTestnet ? "gETH" : "ETH"}</div>
+                    <div style={{ fontSize: ".6em" }}>{isTestnet ? "goerli" : "mainnet"}</div>
+                  </div>
+                ) : (
+                  <div className="wallet-main__asset-display--label">
+                    <div>Create an ETH wallet.</div>
+                  </div>
+                )}
+                <FaEthereum className="wallet-main__asset-display--ethereum-logo" />
+              </div>
+            </IconContext.Provider>
           </div>
 
           <div className="interface__block">
             <div className="interface__block-cell interface__block-cell--space-between">
               <div className="title-font title-font--large">
-                <div className="title__subtitle">Create your private & public key pair.</div>
+                <div className="title__subtitle">Your crypto journey starts here.</div>
                 <div style={{ display: "inline-block" }} className="purple-font">
-                  🧙Key
+                  🏠Your
                 </div>{" "}
-                Creation
+                Wallet
               </div>
               <MdMenu className="icon" />
             </div>
@@ -494,7 +523,9 @@ function Main() {
             <div className="interface__block-cell"></div>
             <div className="interface__block-cell interface__block-cell__footer">
               <TbRefresh className="icon" />
+              {isTestnet ? <BsHddNetwork onClick={() => setIsTestnet(!isTestnet)} className="icon" /> : <BsHddNetworkFill onClick={() => setIsTestnet(!isTestnet)} className="icon" />}
               <div className="icon">ARTSNL</div>
+              <BsReception4 className="icon" />
               <MdLibraryBooks className="icon" />
             </div>
           </div>
