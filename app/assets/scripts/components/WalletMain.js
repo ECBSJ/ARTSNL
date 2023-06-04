c107d4e6c0e8c030820fec5194d69a239649cc5d
ECBSJ <crypto67sauce@gmail.com>
2023-06-04 14:08:58 +0800

    receive and send views

--- app/assets/scripts/components/WalletMain.js
+++ app/assets/scripts/components/WalletMain.js
@@ -6,12 +6,13 @@ import * as ecc from "tiny-secp256k1"
 import * as bitcoin from "../../../../bitcoinjs-lib"
 import { ethers } from "ethers"
 import { Tooltip } from "react-tooltip"
-
+import QRCode from "react-qr-code"
+import { CopyToClipboard } from "react-copy-to-clipboard"
 import { IconContext } from "react-icons"
 import { FaBitcoin, FaEthereum } from "react-icons/fa"
 import { BsHddNetworkFill, BsHddNetwork, BsReception1, BsReception4 } from "react-icons/bs"
-import { MdNavigateNext, MdMenu, MdLibraryBooks, MdCopyAll } from "react-icons/md"
-import { TbRefresh } from "react-icons/tb"
+import { MdNavigateNext, MdMenu, MdLibraryBooks, MdCopyAll, MdOutlineArrowCircleRight } from "react-icons/md"
+import { TbRefresh, TbWalletOff } from "react-icons/tb"
 
 // IMPORT REACT COMPONENTS
 import WalletMain_AssetDisplay from "./WalletMain_AssetDisplay"
@@ -48,6 +49,20 @@ function WalletMain() {
   }
 
   const [openFunctionView, setOpenFunctionView] = useState(0)
+
+  const static_btc_address = "19G4UV3YDkTYj4G3XSYeUkzp4Ew6voQFiR"
+
+  function handleCopyPopup() {
+    document.querySelector(".icon-copy").classList.toggle("icon")
+    document.querySelector(".icon-copy").classList.toggle("icon-copy--active")
+
+    setTimeout(() => {
+      document.querySelector(".icon-copy").classList.toggle("icon")
+      document.querySelector(".icon-copy").classList.toggle("icon-copy--active")
+    }, 1000)
+  }
+
+  let hasFunds = false
 
   return (
     <>
@@ -89,13 +104,50 @@ function WalletMain() {
             <div onClick={() => setOpenFunctionView(1)} className={"snapshot__function-titlebar snapshot__function-titlebar--orange " + (openFunctionView == 1 ? "snapshot__function-titlebar--orange--active" : "")}>
               RECEIVE
             </div>
-            <div className={"snapshot__function-content " + (openFunctionView == 1 ? "snapshot__function-content--display" : "snapshot__function-content--hide")}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quidem commodi error libero voluptatem impedit at, facilis sequi praesentium maiores labore velit! Porro dolore corrupti exercitationem blanditiis! Neque, odio laudantium.</div>
+            <div className={"snapshot__function-content " + (openFunctionView == 1 ? "snapshot__function-content--display" : "snapshot__function-content--hide")}>
+              <div>Deposit BTC Here</div>
+              <div style={{ fontSize: ".5rem", color: "gray", width: "80%", textAlign: "justify" }}>Fund your bitcoin wallet by depositing funds to the QR code below. Or copy & paste the address string shown below the QR code.</div>
+              <div style={{ padding: "15px 0 15px 0" }}>
+                <QRCode bgColor="#FFA500" fgColor="#131a2a" style={{ height: "150px", width: "150px" }} value={static_btc_address} />
+              </div>
+              <div style={{ fontSize: ".6rem", paddingBottom: "3px" }} className="display-flex">
+                <CopyToClipboard text={static_btc_address} onCopy={() => handleCopyPopup()}>
+                  <MdCopyAll style={{ width: "20px", height: "20px" }} className="icon icon-copy" />
+                </CopyToClipboard>
+                {static_btc_address}
+              </div>
+              <div style={{ fontSize: ".5rem", color: "red", width: "80%", textAlign: "justify" }}>This BTC address is a P2PKH address. Always confirm the receiving address before broadcasting transaction. Sending BTC to the wrong address will result in loss of funds.</div>
+            </div>
           </div>
           <div className="snapshot__function-wrapper">
             <div onClick={() => setOpenFunctionView(2)} className={"snapshot__function-titlebar snapshot__function-titlebar--orange " + (openFunctionView == 2 ? "snapshot__function-titlebar--orange--active" : "")}>
               SEND
             </div>
-            <div className={"snapshot__function-content " + (openFunctionView == 2 ? "snapshot__function-content--display" : "snapshot__function-content--hide")}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In enim aliquid cupiditate magni a magnam, esse vitae soluta! Qui vero aspernatur eaque earum unde id odit dolorum quasi molestias beatae!</div>
+            <div className={"snapshot__function-content " + (openFunctionView == 2 ? "snapshot__function-content--display" : "snapshot__function-content--hide")}>
+              {hasFunds ? (
+                <>
+                  <div style={{ paddingBottom: "10px" }}>Start DIY TX</div>
+                  <MdOutlineArrowCircleRight style={{ width: "80px", height: "80px" }} className="icon" />
+                  <div style={{ width: "80%", fontSize: ".56rem", color: "gray", textAlign: "justify", paddingTop: "10px" }}>
+                    <p>&#x2022;Before proceeding to the DIY transaction process, please have a valid P2PKH receiving address ready.</p>
+                    <p>&#x2022;You still can leave BTC in this wallet as the browser&#39;s secure storage will store your credentials.</p>
+                    <p>&#x2022;Highly advised to not leave large sums of value in this wallet. This wallet is still in beta production.</p>
+                    <p>&#x2022;If you want, you can always backup or move your key pair by navigating to the Export option in the Menu.</p>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div style={{ paddingBottom: "10px" }}>
+                    You do not have <br /> any BTC to send.
+                  </div>
+                  <TbWalletOff style={{ width: "80px", height: "80px" }} className="icon" />
+                  <div style={{ width: "80%", fontSize: ".56rem", color: "gray", textAlign: "justify", paddingTop: "10px" }}>
+                    <p>&#x2022;Navigate to the Receive tab to fund your BTC wallet in order to construct a transaction.</p>
+                    <p>&#x2022;If you don&#39;t want to fund this wallet with mainnet UTXOs, you can switch to the testnet to use tBTC.</p>
+                  </div>
+                </>
+              )}
+            </div>
           </div>
         </div>
       </div>
