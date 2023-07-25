9d0f9db22614c24c5be3ce55add0f7ae5da743ec
ECBSJ <crypto67sauce@gmail.com>
2023-07-25 16:23:31 +0800

    sign inputs style complete

--- app/assets/scripts/components/btctx/BtcTxSignInputs.js
+++ app/assets/scripts/components/btctx/BtcTxSignInputs.js
@@ -5,6 +5,7 @@ import DispatchContext from "../../DispatchContext"
 import { IconContext } from "react-icons"
 import { BsFileEarmarkLock2Fill } from "react-icons/bs"
 import { TbSignatureOff } from "react-icons/tb"
+import { FaSignature } from "react-icons/fa"
 
 import UtxoDisplayCard from "./UtxoDisplayCard"
 
@@ -15,7 +16,7 @@ function BtcTxSignInputs({ setTxStatus }) {
   let utxoData_Array = [
     { txid: "a296be122cc5c90bfc7e50f65b2c2e12d231a761d69ff05ec8a05b48f6f16b9a", vout: 0, status: { confirmed: true, block_height: 2434362, block_hash: "000000000000000588988168cfb4f924fcd912f6a7c9d909fbd978067be31f01", block_time: 1684590437 }, value: 5800 },
     { txid: "be7610fcaad261bf04241854c3cc41625283833fb5f09f56648c3769741eaf02", vout: 1, status: { confirmed: true, block_height: 2443296, block_hash: "0000000000000005a06960c65d7b9eeec50e358be78b64737ea920f90a3975ce", block_time: 1690082989 }, value: 8158 },
-    { txid: "9153e5420b1092ff65d90a028df8840e0e3dfc8b9c8e1c1c0664e02f000c5def", vout: 0, status: { confirmed: true, block_height: 2434520, block_hash: "000000000000000f4632a88a45d61cd4e777040fc0203108661e7ebedcddc4bb", block_time: 1684648693 }, value: 13700 }
+    { txid: "9153e5420b1092ff65d90a028df8840e0e3dfc8b9c8e1c1c0664e02f000c5def", vout: 0, status: { confirmed: true, block_height: 2434520, block_hash: "000000000000000f4632a88a45d61cd4e777040fc0203108661e7ebedcddc4bb", block_time: 1684648693 }, value: 13700 },
   ]
 
   let selectedUtxo = [0, 1, 2]
@@ -25,11 +26,26 @@ function BtcTxSignInputs({ setTxStatus }) {
   let translateXConstant = -419
   let translateXAmount = translateXConstant * translateXMultiplier
   let translateXStyle = {
-    translate: translateXAmount.toString() + "px"
+    translate: translateXAmount.toString() + "px",
   }
 
   function handleSelect(e, index) {
     setTranslateXMultipler(index)
+  }
+
+  const [isSigned_Array, setIsSigned_Array] = useState([])
+
+  // called in <UtxoDisplayCard />
+  function handleSigning(e, index) {
+    if (isSigned_Array.includes(index)) {
+      // if already signed, do nothing
+      null
+    } else {
+      // if not signed, then sign
+      setTimeout(() => {
+        setIsSigned_Array([...isSigned_Array, index])
+      }, 1000)
+    }
   }
 
   return (
@@ -39,11 +55,11 @@ function BtcTxSignInputs({ setTxStatus }) {
           <div className="tx-builder__overlay__outer">Step 5: Sign Inputs</div>
 
           <div className="tx-builder__blueprint">
-            <span style={{ position: "absolute", top: "4px", right: "7px", fontSize: "0.5em" }}>UTXOs pending signature: {"0"}</span>
+            <span style={{ position: "absolute", top: "4px", right: "7px", fontSize: "0.5em" }}>UTXOs pending signature: {selectedUtxo.length - isSigned_Array.length}</span>
             <div className="tx-builder__blueprint-signing-section__container-top">
               {selectedUtxo.map((selectedUtxo, index) => {
                 return (
-                  <button key={index} onClick={e => handleSelect(e, index)} className={"put-capsule " + (translateXMultiplier === index ? "put-capsule--selected" : "")}>
+                  <button key={index} onClick={(e) => handleSelect(e, index)} className={"put-capsule " + (translateXMultiplier === index ? "put-capsule--selected" : "")}>
                     <BsFileEarmarkLock2Fill style={{ marginRight: "6px" }} />
                     <div style={{ textAlign: "left", fontSize: "0.9em" }}>
                       <span>UTXO: ...{utxoData_Array[selectedUtxo].txid.slice(-4)}</span>
@@ -52,7 +68,7 @@ function BtcTxSignInputs({ setTxStatus }) {
                       <br />
                       <span>Index: &#91;{index}&#93;</span>
                     </div>
-                    <TbSignatureOff style={{ color: "red" }} />
+                    {isSigned_Array.includes(index) ? <FaSignature style={{ color: "greenyellow" }} /> : <TbSignatureOff style={{ color: "red" }} />}
                   </button>
                 )
               })}
@@ -62,7 +78,7 @@ function BtcTxSignInputs({ setTxStatus }) {
                 {selectedUtxo.map((selectedIndex, index) => {
                   let utxo = utxoData_Array[selectedIndex]
 
-                  return <UtxoDisplayCard key={index} index={index} txid={utxo.txid} vout={utxo.vout} confirmed={utxo.status.confirmed} block_height={utxo.status.block_height} block_hash={utxo.status.block_hash} block_time={utxo.status.block_time} value={utxo.value} selectedArray={someArray} isSigning={true} />
+                  return <UtxoDisplayCard key={index} index={index} txid={utxo.txid} vout={utxo.vout} confirmed={utxo.status.confirmed} block_height={utxo.status.block_height} block_hash={utxo.status.block_hash} block_time={utxo.status.block_time} value={utxo.value} selectedArray={someArray} isSigning={true} handleSigning={handleSigning} isSigned_Array={isSigned_Array} />
                 })}
               </div>
             </div>
--- app/assets/scripts/components/btctx/UtxoDisplayCard.js
+++ app/assets/scripts/components/btctx/UtxoDisplayCard.js
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from "react"
 import { MdIndeterminateCheckBox, MdCheckBox } from "react-icons/md"
 
-function UtxoDisplayCard({ index, txid, vout, confirmed, block_height, block_hash, block_time, value, pushIndexToSelectedArray, selectedArray, isSigning }) {
+function UtxoDisplayCard({ index, txid, vout, confirmed, block_height, block_hash, block_time, value, pushIndexToSelectedArray, selectedArray, isSigning, handleSigning, isSigned_Array }) {
   // green checkmark icon will appear on bottom right of card when isSelected is true
   const [isSelected, setIsSelected] = useState(false)
 
-  function handleSelect() {
-    // called from <BitcoinTxBuilder />
-    pushIndexToSelectedArray(index)
-    setIsSelected(!isSelected)
+  function handleSelect(e, index) {
+    if (isSigning) {
+      // for signing utxo
+      document.getElementById("sign-button-grab" + index).innerText = "Signing..."
+      handleSigning(e, index)
+    } else {
+      // for selecting utxo as inputs
+      // called from <BitcoinTxBuilder />
+      pushIndexToSelectedArray(index)
+      setIsSelected(!isSelected)
+    }
   }
 
   // to sustain isSelected boolean when user switches back from <ConfirmSelectedUtxo /> to <UtxoCarousel />
@@ -20,29 +27,52 @@ function UtxoDisplayCard({ index, txid, vout, confirmed, block_height, block_has
 
   return (
     <>
-      <div onClick={() => handleSelect()} className={"utxo__display-card " + (isSelected ? "utxo__display-card--selected" : "") + (isSigning ? "utxo__display-card--smaller" : "")}>
-        <span>&#123;</span>
-        <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>txid&#58; &#34;{txid.slice(0, 12) + "..." + txid.slice(-12)}&#34;,</span>
-        <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>vout&#58; {vout},</span>
-        <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>status&#58; &#123;</span>
-        <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>confirmed&#58; {confirmed ? "true" : "false"},</span>
-        <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>block_height&#58; {block_height},</span>
-        <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>block_hash&#58; &#34;{block_hash.slice(0, 21) + "..." + block_hash.slice(-10)}&#34;,</span>
-        <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>block_time&#58; {block_time}</span>
-        <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>&#125;,</span>
-        <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>value&#58; {value}</span>
-        <br />
-        <span>&#125;</span>
-        {isSigning ? "" : <>{isSelected ? <MdCheckBox style={{ position: "absolute", bottom: "0", right: "0", color: "lightGreen" }} className="icon icon--position-absolute" /> : <MdIndeterminateCheckBox style={{ position: "absolute", bottom: "0", right: "0", color: "white" }} className="icon icon--position-absolute" />}</>}
+      <div onClick={(e) => handleSelect(e, index)} className={"utxo__display-card " + (isSelected ? "utxo__display-card--selected" : "") + (isSigning ? "utxo__display-card--smaller" : "")}>
+        {isSigned_Array.includes(index) ? (
+          <>
+            <div className="utxo__display-card--signed-cover">
+              <span style={{ fontSize: "1.3rem" }} className="font--russo-one">
+                UTXO Verified & Signed!
+              </span>
+              <br />
+              <span>
+                ScriptSig: <br /> {"ScriptSig here."}
+              </span>
+            </div>
+          </>
+        ) : (
+          <>
+            {isSigning ? (
+              <button id={"sign-button-grab" + index} className="utxo__display-card--smaller__button">
+                Sign UTXO
+              </button>
+            ) : (
+              ""
+            )}
+            <span>&#123;</span>
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>txid&#58; &#34;{txid.slice(0, 12) + "..." + txid.slice(-12)}&#34;,</span>
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>vout&#58; {vout},</span>
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>status&#58; &#123;</span>
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>confirmed&#58; {confirmed ? "true" : "false"},</span>
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>block_height&#58; {block_height},</span>
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>block_hash&#58; &#34;{block_hash.slice(0, 21) + "..." + block_hash.slice(-10)}&#34;,</span>
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>block_time&#58; {block_time}</span>
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>&#125;,</span>
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>value&#58; {value}</span>
+            <br />
+            <span>&#125;</span>
+            {isSigning ? "" : <>{isSelected ? <MdCheckBox style={{ position: "absolute", bottom: "0", right: "0", color: "lightGreen" }} className="icon icon--position-absolute" /> : <MdIndeterminateCheckBox style={{ position: "absolute", bottom: "0", right: "0", color: "white" }} className="icon icon--position-absolute" />}</>}
+          </>
+        )}
       </div>
     </>
   )
